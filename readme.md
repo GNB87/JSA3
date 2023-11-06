76032361b8f241c087e486c172f7da5661a1c27c
Gabriela Benavides <gabrielarussell@my.whccd.rdu>
2023-11-05 20:30:17 -0800

    first commit

---
+++ readme.md
@@ -0,0 +1,2 @@
+Auther:Gabriela Benavides 
+Purpose: JS3 Assignment and so far
---
+++ script3.js
@@ -0,0 +1,27 @@
+// 3. Write code that creates an 8x8 grid, similar to a chessboard, using alternating spaces and the pound/hashtag symbol (#).
+// Hint: There are two distinct alternating patterns here…
+// Example Output:
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+
+let size = 8;
+let board = "";
+
+for (let y = 0; y < size; y++) {
+  for (let x = 0; x < size; x++) {
+    if ((x + y) % 2 == 0) { 
+      board += " ";
+    } else {
+      board += "#";
+    }
+  }
+  board += "\n";
+}
+
+console.log(board);
